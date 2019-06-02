46f5228eb9b5db5e8f13b7641cecd4d946e9c971
cchowgule <14945536+cchowgule@users.noreply.github.com>
2019-06-01 21:00:19 -0400

    delete previous PS answers

--- PS1/PS1.P1.js
+++
@@ -1,5 +0,0 @@
-const alphaSort = str => str.split('').sort().join('');
-
-const testStr = 'supercalifragilisticexpialidocious';
-
-console.log(`${testStr} listed alphabetically is: ${alphaSort(testStr)}`);
